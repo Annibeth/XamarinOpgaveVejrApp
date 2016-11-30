b376098a5f9a690ae8ea508424654b1eae0cb27e
Admin <Admin>
2016-11-30 15:01:50 +0100

    Vejr Opgave

---
+++ VejrApp/VejrApp/VejrApp/VejrAppPage1.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VejrApp
+{
+    public partial class VejrAppPage1 : ContentPage
+    {
+        public VejrAppPage1()
+        {
+            InitializeComponent();
+        }
+    }
+}
